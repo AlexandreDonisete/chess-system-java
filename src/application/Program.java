8b590738c197239adcde50d873c24a9a28556c22
Alexandre Donisete <ale.bezerra.donisete@gmail.com>
2025-05-01 22:35:11 -0300

    Movimento Especial Promoção

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,12 @@ public class Program {
                 if(capturedPiece != null) {
                     captured.add(capturedPiece);
                 }
+                if(chessMatch.getPromoted() != null) {
+                    System.out.println("Digite para qual peça deseja promover (B/N/R/Q): ");
+                    String type = sc.nextLine().toUpperCase();
+                    chessMatch.replacePromotedPiece(type);
+                }
+
             } catch (ChessException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
